65daa84fddb168647b9b2a54e17590cab96396d8
soongu <naong526@naver.com>
2022-12-31 11:24:53 +0900

    # 배포 설정

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -8,6 +8,8 @@ console.log('현재호스트: ', hostname);
 let backendHost; // 백엔드 호스트 이름
 if (hostname === 'localhost') {
     backendHost = 'http://localhost:8181';
+} else if (hostname === 'todo-react-front-app.s3-website.ap-northeast-2.amazonaws.com') {
+    backendHost = 'http://ec2-54-180-12-218.ap-northeast-2.compute.amazonaws.com';
 }
 
 export const API_BASE_URL = `${backendHost}`;
